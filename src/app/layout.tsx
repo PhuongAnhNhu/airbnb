ebfcbca10b55157e335e0b10baed749d0a2d227c
Phuong Anh <nhuphuonganh001@..com>
2023-11-22 21:30:27 +0100

    set up tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
+
+import { Nunito } from "next/font/google";
+import '../../styles/globals.css'
 export const metadata = {
-title: 'Airbnb' 
+    title: 'Airbnb'
 }
 
-
-export default function RootLayout  ({ children,} : { children: React.ReactNode})
-{
-  return (
-    <html lang='en'>{children}</html>
-  )
+const font = Nunito({
+    subsets: ["latin"],
+})
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+    return (
+        <html lang='en'>
+            <body className={font.className}>
+                {children} 
+            </body>
+        </html>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-rose-700 text-2xl'>Hello Airbnb</div>
   )
 }
 
