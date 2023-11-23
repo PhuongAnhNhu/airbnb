5d5f52984fcd71479fdf6b6af8d65e8147f94022
Phuong Anh <nhuphuonganh001@..com>
2023-11-23 20:15:10 +0100

    Create container component

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Container from '../Container'
 
 const Navbar = () => {
     return (
         <div className='fixed w-full bg-white z-10 shadow-sm'>
-            <div className="py-4 border-b-[1px]"></div>
+            <div className="py-4 border-b-[1px]">
+                <Container>Container</Container>
+            </div>
         </div>
     )
 }
