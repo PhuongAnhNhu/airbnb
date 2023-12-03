d164360355e010ba719f371e69337c8077a972df
Phuong Anh <nhuphuonganh001@..com>
2023-12-03 14:29:53 +0100

    Modal component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hellooo" isOpen />
+          <Modal actionLabel="Submit" title="Hellooo" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
