e632f00cb18f136641c8c243fc25895b1dc92acd
Phuong Anh <nhuphuonganh001@..com>
2023-12-12 22:45:38 +0100

    UI Register

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -1,7 +1,6 @@
 'use client';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
@@ -9,6 +8,8 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
+import Button from '../Button';
+import { BiLogoGithub } from 'react-icons/bi';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -49,6 +50,21 @@ const RegisterModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}}></Button>
+      <Button outline label="Continue with Github" icon={BiLogoGithub} onClick={() => {}}></Button>
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div className="">Already have an account?</div>
+          <div className="text-neutral-800 cursor-pointer hover:underline" onClick={registerModal.onClose}>
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
@@ -58,6 +74,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
